20bbb04573a2a69cbbd597a558e47d94ea66de5c
SkylerMax <skyler@adcodedesign.com>
2023-05-29 20:39:31 +0800

    ee-custom-elements- create

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Plugin Name: Breakdance Custom Elements
- * Plugin URI: https://breakdance.com/
+ * Plugin Name: Exotux Estate Custom Elements
+ * Plugin URI: https://exotic-estate.com/
  * Description: ALPHA - NOT TO BE USED IN PRODUCTION
- * Author: Breakdance
- * Author URI: https://breakdance.com/
+ * Author: Skyler
+ * Author URI: https://exotic-estate.com/
  * License: GPLv2
  * Text Domain: breakdance
  * Domain Path: /languages/
@@ -16,31 +16,33 @@ namespace BreakdanceCustomElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
 
-add_action('breakdance_loaded', function () {
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
-        'BreakdanceCustomElements',
-        'element',
-        'Custom Elements',
-        false
-    );
+add_action(
+    'breakdance_loaded',
+    function () {
+        \Breakdance\ElementStudio\registerSaveLocation(
+            getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
+            'BreakdanceCustomElements',
+            'element',
+            'Custom Elements',
+            false
+        );
 
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
-        'BreakdanceCustomElements',
-        'macro',
-        'Custom Macros',
-        false,
-    );
+        \Breakdance\ElementStudio\registerSaveLocation(
+            getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
+            'BreakdanceCustomElements',
+            'macro',
+            'Custom Macros',
+            false,
+        );
 
-    \Breakdance\ElementStudio\registerSaveLocation(
-        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
-        'BreakdanceCustomElements',
-        'preset',
-        'Custom Presets',
-        false,
-    );
-},
+        \Breakdance\ElementStudio\registerSaveLocation(
+            getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
+            'BreakdanceCustomElements',
+            'preset',
+            'Custom Presets',
+            false,
+        );
+    },
     // register elements before loading them
     9
 );
